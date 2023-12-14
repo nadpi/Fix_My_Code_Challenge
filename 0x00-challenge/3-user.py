c5f8ed82d64182f1d197146010c6bba1bbeb4501
nadpi <nadawaled87@gmail.com>
2023-12-14 09:58:49 -0800

    [3. User password]

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -84,20 +84,20 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if user_1.is_valid_password(u_pwd):
+    if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
-    if user_1.is_valid_password("Fakepwd"):
+    if not user_1.is_valid_password("Fakepwd"):
         print("is_valid_password should return False if it's not the right \
 password")
 
-    if user_1.is_valid_password(None):
+    if not user_1.is_valid_password(None):
         print("is_valid_password should return False if compare with None")
 
-    if user_1.is_valid_password(89):
+    if not user_1.is_valid_password(89):
         print("is_valid_password should return False if compare with integer")
 
-    if user_2.is_valid_password("No pwd"):
+    if not user_2.is_valid_password("No pwd"):
         print("is_valid_password should return False if no password set \
 before")
